a9341b79470cebf17208fbe7430848f46495331f
phungvanquyet94 <phungvanquyet94@gmail.com>
2020-04-06 00:51:22 +0700

    quyet.phung fix pars.int

--- src/easy/problem1/solution3/solution.js
+++ src/easy/problem1/solution3/solution.js
@@ -1,6 +1,6 @@
 const run = (arr, k) => {
     for (let i = 0, size = arr.length; i < size - 1; i += 1) {
-        if (arr.slice(i + 1).includes(k - arr[parseInt(i, 10)])) return true;
+        if (arr.slice(i + 1).includes(k - arr[i])) return true;
     }
     return false;
 };
