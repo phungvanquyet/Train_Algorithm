634853d9f76202bcfbfb14b0f9370b46a86a4a85
PhamMinhHaiAu-12035071 <phamau1994x@gmail.com>
2020-04-05 16:07:27 +0700

    Add data test cases.

--- src/tests/easy/problem1/solution.test.js
+++ src/tests/easy/problem1/solution.test.js
@@ -1,5 +1,7 @@
+/* eslint security/detect-non-literal-require: 0 */
 const glob = require('glob');
 const _ = require('lodash');
+const dataPositiveNumbers = require('./data/number_int.json');
 
 const getListFuncProblem = path => {
     return _.flow([_.partialRight(_.map, item => require(item))])(glob.sync(path));
@@ -7,9 +9,11 @@ const getListFuncProblem = path => {
 describe('TestCase Easy', () => {
     describe('TestCase Problem 1', () => {
         const listObj = getListFuncProblem(`${process.cwd()}/src/easy/problem1/**/solution.js`);
-        it('Test Success', async () => {
+        it('Test with positive numbers', async () => {
             _.forEach(listObj, func => {
-                expect(func([10, 15, 3, 7], 17)).toBeTruthy();
+                _.forEach(_.values(dataPositiveNumbers), item => {
+                    expect(func(item.input.arr, item.input.k)).toBe(item.output);
+                });
             });
         });
     });
