588a84021e65c9bd1e0d3156ea31446c45d1591b
PhamMinhHaiAu-12035071 <phamau1994x@gmail.com>
2020-04-08 20:53:28 +0700

    Fix two add float numbers.

--- src/easy/problem1/solution1/solution.js
+++ src/easy/problem1/solution1/solution.js
@@ -1,9 +1,20 @@
+const isFloat = require('./is_float');
+
 const run = (arr, k) => {
     for (let i = 0, size = arr.length; i < size - 1; i += 1) {
         for (let j = i + 1; j < size; j += 1) {
-            if (arr[parseInt(i, 10)] + arr[parseInt(j, 10)] === k) return true;
+            const total = arr[parseInt(i, 10)] + arr[parseInt(j, 10)];
+            if (isFloat(total) && isFloat(k)) {
+                const getLengthDecimal = Number(k)
+                    .toString()
+                    .split('.')[1].length;
+                const roundTotal = parseFloat(Number(total).toFixed(getLengthDecimal));
+                if (roundTotal === k) return true;
+            } else if (arr[parseInt(i, 10)] + arr[parseInt(j, 10)] === k) return true;
         }
     }
     return false;
 };
+const result = run([0.1, 0.2], 0.3);
+console.log(result);
 module.exports = run;
