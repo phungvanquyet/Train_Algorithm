7c2a16434d644a5285d46df180f46119a6e930c6
PhamMinhHaiAu-12035071 <phamau1994x@gmail.com>
2020-04-10 10:26:32 +0700

    Add benchmark.

--- benchmark/hard/problem1/solution.benchmark.js
+++ benchmark/hard/problem1/solution.benchmark.js
@@ -3,20 +3,16 @@ const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite();
 const _ = require('lodash');
 const { getListObjectProblem } = require('../../../src/helper/utils');
-const dataIntNumber = require('../../../src/tests/easy/problem1/data/int_number.json');
-const dataFloatNumber = require('../../../src/tests/easy/problem1/data/float_number.json');
+const data = require('../../../src/tests/hard/problem1/data/sample.json');
 
-const listObj = getListObjectProblem(`${process.cwd()}/src/easy/problem1/**/solution.js`);
+const listObj = getListObjectProblem(`${process.cwd()}/src/hard/problem1/**/solution.js`);
 const main = () => {
     _.forEach(listObj, value => {
         suite.add(
             `Thư mục ${value.dir} với mode ${value.mode} trong file solution.js với function ${value.nameFunc}`,
             () => {
-                _.forEach(_.values(dataIntNumber), item => {
-                    value.run(item.input.arr, item.input.k);
-                });
-                _.forEach(_.values(dataFloatNumber), item => {
-                    value.run(item.input.arr, item.input.k);
+                _.forEach(_.values(data), item => {
+                    value.run(item.input);
                 });
             },
         );
--- src/helper/utils.js
+++ src/helper/utils.js
@@ -2,7 +2,31 @@
 const glob = require('glob');
 const _ = require('lodash');
 
+const getMode = (arr, mode = ['easy', 'medium', 'hard']) => {
+    for (let i = 0, size = arr.length; i < size; i += 1) {
+        if (_.findIndex(mode, el => el === arr[parseInt(i, 10)]) !== -1) {
+            return arr[parseInt(i, 10)];
+        }
+    }
+    return 'unknown';
+};
 module.exports = {
+    getMode,
+    getListObjectProblem(path) {
+        return _.flow([
+            _.partialRight(_.map, item =>
+                _.assign(
+                    {},
+                    {
+                        dir: _.split(item, '/')[_.split(item, '/').length - 2],
+                        mode: getMode(_.split(item, '/')),
+                        run: require(item),
+                        nameFunc: require(item).name,
+                    },
+                ),
+            ),
+        ])(glob.sync(path));
+    },
     getListFuncProblem(path) {
         return _.flow([_.partialRight(_.map, item => require(item))])(glob.sync(path));
     },
